48e0c1b5be535f1bb4622e6ff024587f6f9cc935
MATTHEW.GREENWOOD <s4699133L@student.uq.edu.au>
2023-05-24 19:41:17 +1000

    PathTest file created and set up.

---
+++ test/lms/logistics/PathTest.java
@@ -0,0 +1,46 @@
+package lms.logistics;
+
+import static org.junit.Assert.*;
+
+public class PathTest {
+
+    @org.junit.Test
+    public void head() {
+    }
+
+    @org.junit.Test
+    public void tail() {
+    }
+
+    @org.junit.Test
+    public void getPrevious() {
+    }
+
+    @org.junit.Test
+    public void getNext() {
+    }
+
+    @org.junit.Test
+    public void setPrevious() {
+    }
+
+    @org.junit.Test
+    public void setNext() {
+    }
+
+    @org.junit.Test
+    public void getNode() {
+    }
+
+    @org.junit.Test
+    public void applyAll() {
+    }
+
+    @org.junit.Test
+    public void testToString() {
+    }
+
+    @org.junit.Test
+    public void testEquals() {
+    }
+}
